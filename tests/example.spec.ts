155316db98551a80b8f169fcfa2448561268f1f0
Ritesh Patel <ccaerpa@ucl.ac.uk>
2024-09-02 00:15:54 +0100

    first playwright test - headless mode

---
+++ tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+
+//to run tests npx playwright test
+
+test('Simple basic test', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    const pageTitle = await page.locator('h1')
+    await expect(pageTitle).toContainText('Example Domain')
+  })
